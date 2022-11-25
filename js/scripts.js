c348175f1a4bbbcf6db06c286957d36c5d0f7f4d
Joseph de Garr Wilkinson <joseph.degarr@googlemail.com>
2022-11-25 16:50:15 +0100

    pokemon list with for loop

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,25 @@
-var size = 100;
-var doubleSize = size * 2;
-var minSize = (doubleSize * 2) - (size / 2);
-document.write(minSize);
+let pokemonList = [
+    {
+        name: "Squirtle", 
+        height: 0.5, 
+        type: "water"
+    }, 
+    {
+        name: "Charizard", 
+        height: 1.7, 
+        type: ["fire","flying"]
+    }, 
+    {
+        name: "Geodude", 
+        height: 0.4, 
+        type: ["rock","ground"]
+    }
+];
+
+for (let i = 0; i < pokemonList.length; i++) {
+    document.write
+    (pokemonList[i].name+" "+"(Height: "+pokemonList[i].height+")"+" ");
+if (pokemonList[i].height >= 0.6) {
+    document.write('(bigboi) ')
+}
+  };
